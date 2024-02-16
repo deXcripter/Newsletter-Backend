e3374fcae07b3b2155eb62789c7a587f5625e48d
Johnpaul <indisputable.jay@gmail.com>
2024-02-16 07:25:02 +0100

    finsihed with the controllers

--- dest/controllers/user-controller.js
+++ dest/controllers/user-controller.js
@@ -1,7 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAllSubscribers = exports.subscribe = void 0;
-const subscribe = () => { };
+const user_model_1 = __importDefault(require("../models/user-model"));
+const appError_1 = __importDefault(require("../utils/appError"));
+const subscribe = async (req, res, next) => {
+    const { email } = req.body;
+    if (!email)
+        return next(new appError_1.default('Enter your email', 401));
+    const user = await user_model_1.default.create(email);
+    res
+        .status(201)
+        .json({ status: 'success', message: 'Email added', data: { user } });
+};
 exports.subscribe = subscribe;
-const getAllSubscribers = () => { };
+const getAllSubscribers = async (req, res, next) => {
+    const users = await user_model_1.default.find();
+    res.status(200).json({ status: 'success', data: { users } });
+};
 exports.getAllSubscribers = getAllSubscribers;
--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,5 +1,19 @@
 import { RequestHandler } from 'express';
 import User from '../models/user-model';
+import { iBody } from '../utils/interface';
+import appError from '../utils/appError';
 
-export const subscribe: RequestHandler = () => {};
-export const getAllSubscribers: RequestHandler = () => {};
+export const subscribe: RequestHandler = async (req, res, next) => {
+  const { email } = req.body as iBody;
+  if (!email) return next(new appError('Enter your email', 401));
+
+  const user = await User.create(email);
+  res
+    .status(201)
+    .json({ status: 'success', message: 'Email added', data: { user } });
+};
+
+export const getAllSubscribers: RequestHandler = async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({ status: 'success', data: { users } });
+};
