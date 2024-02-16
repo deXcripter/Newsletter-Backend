c7a183d83c7885dea794db1bf3b0bf72cad11f8c
Johnpaul <indisputable.jay@gmail.com>
2024-02-16 07:19:35 +0100

    added subscribe and unsubscribe controllers

--- dest/controllers/user-controller.js
+++ dest/controllers/user-controller.js
@@ -1,22 +1,7 @@
-'use strict';
-var __importDefault = (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod };
-    };
-Object.defineProperty(exports, '__esModule', { value: true });
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAllSubscribers = exports.subscribe = void 0;
-const user_model_1 = __importDefault(require('../models/user-model'));
-const appError_1 = __importDefault(require('../utils/appError'));
-const subscribe = async (req, res, next) => {
-    const { email } = req.body;
-    if (!email)
-        return next(new appError_1.default('Please enter your email', 401));
-    const user = await user_model_1.default.create(email);
-    res.status(200).json({ status: 'success', data: { user } });
-};
+const subscribe = () => { };
 exports.subscribe = subscribe;
-const getAllSubscribers = async (req, res, next) => {
-    const users = user_model_1.default.find();
-    res.status(200).json({ status: 'success', data: { users } });
-};
+const getAllSubscribers = () => { };
 exports.getAllSubscribers = getAllSubscribers;
