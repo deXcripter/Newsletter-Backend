a409b50eb270464d345b145b54298ef57e09b9cd
Johnpaul <indisputable.jay@gmail.com>
2024-02-19 01:49:05 +0100

    without the js

--- dest/routes/user-route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const user_controller_1 = require("../controllers/user-controller");
-const express_1 = require("express");
-const Route = (0, express_1.Router)();
-Route.get('/', user_controller_1.getAllSubscribers);
-Route.post('/', user_controller_1.subscribe);
-Route.patch('/', user_controller_1.unSubscribe);
-exports.default = Route;
